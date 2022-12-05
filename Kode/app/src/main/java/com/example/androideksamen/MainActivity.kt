4f167fa482a3711931111b6400a36defced1bf0d
Simen Hansen <siha059@student.kristiania.no>
2022-12-05 16:35:25 +0100

    download data via search input and button

--- Kode/app/src/main/java/com/example/androideksamen/MainActivity.kt
+++ Kode/app/src/main/java/com/example/androideksamen/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.androideksamen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -16,20 +18,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        GlobalScope.launch(Dispatchers.Main) {
-            downloadRecipies()
+        val searchBtn = findViewById<Button>(R.id.search_btn)
+        val searchInput = findViewById<EditText>(R.id.search_input)
+        var userInput: String
+        searchBtn.setOnClickListener{
+            userInput = searchInput.text.toString()
+            GlobalScope.launch(Dispatchers.Main) {
+                downloadRecipes(userInput)
+            }
+            Log.i("testing", "test user input: "+ userInput)
         }
+
     }
 
-    suspend fun downloadRecipies(): ArrayList<RecipeData>{
+    suspend fun downloadRecipes(userInput: String): ArrayList<RecipeData>{
 
         val apiId: String = "2a4625b1"
         val apiKey: String = "ba6e08d3cf44ce2e3e826635508ed960"
+        val searchQuery = userInput
 
         val allData = ArrayList<RecipeData>()
         GlobalScope.async {
-            val assetData = URL("https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${apiId}&app_key=${apiKey}").readText().toString()
+            val assetData = URL("https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=${apiId}&app_key=${apiKey}").readText().toString()
 //            Log.i("testing", "testing api: "+assetData)
             val recipeDataArray = (JSONObject(assetData).get("hits") as JSONArray)
             (0 until recipeDataArray.length()).forEach { recipeNr ->
@@ -43,6 +53,10 @@ class MainActivity : AppCompatActivity() {
             }
 
         }.await()
+
+        allData.forEach { recipe ->
+            Log.i("testing", "test: " + recipe.recipeName)
+        }
         return allData
     }
 }
