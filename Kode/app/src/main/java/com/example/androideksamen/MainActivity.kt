5da3eb9b0fe5ff8840c50ecb788b60fca6aa140e
Simen Hansen <siha059@student.kristiania.no>
2022-12-06 15:36:22 +0100

    added settings and search history activity

--- Kode/app/src/main/java/com/example/androideksamen/MainActivity.kt
+++ Kode/app/src/main/java/com/example/androideksamen/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androideksamen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -20,7 +21,12 @@ class MainActivity : AppCompatActivity() {
 
         val searchBtn = findViewById<Button>(R.id.search_btn)
         val searchInput = findViewById<EditText>(R.id.search_input)
+        val searchHistoryBtn = findViewById<Button>(R.id.search_history_btn)
+        val settingsBtn = findViewById<Button>(R.id.settings_btn)
         var userInput: String
+
+
+        // Button onClick START
         searchBtn.setOnClickListener{
             userInput = searchInput.text.toString()
             GlobalScope.launch(Dispatchers.Main) {
@@ -29,18 +35,25 @@ class MainActivity : AppCompatActivity() {
             Log.i("testing", "test user input: "+ userInput)
         }
 
+        searchHistoryBtn.setOnClickListener {
+            startActivity(Intent(applicationContext, SearchHistoryActivity::class.java))
+        }
+        settingsBtn.setOnClickListener {
+            startActivity(Intent(applicationContext, SettingsActivity::class.java))
+        }
+        // Button onclick END
+
     }
 
     suspend fun downloadRecipes(userInput: String): ArrayList<RecipeData>{
 
         val apiId: String = "2a4625b1"
         val apiKey: String = "ba6e08d3cf44ce2e3e826635508ed960"
-        val searchQuery = userInput
+//        val searchQuery = userInput
 
         val allData = ArrayList<RecipeData>()
         GlobalScope.async {
-            val assetData = URL("https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=${apiId}&app_key=${apiKey}").readText().toString()
-//            Log.i("testing", "testing api: "+assetData)
+            val assetData = URL("https://api.edamam.com/api/recipes/v2?type=public&q=${userInput}&app_id=${apiId}&app_key=${apiKey}").readText().toString()
             val recipeDataArray = (JSONObject(assetData).get("hits") as JSONArray)
             (0 until recipeDataArray.length()).forEach { recipeNr ->
                 val dataItem = RecipeData()
@@ -48,6 +61,7 @@ class MainActivity : AppCompatActivity() {
                 val assetItem = recipeDataArray.get(recipeNr)
                 val recipe = (assetItem as JSONObject).get("recipe")
                 dataItem.recipeName = (recipe as JSONObject).getString("label")
+                dataItem.yield = (recipe as JSONObject).getString("yield")
 
                 allData.add(dataItem)
             }
@@ -56,6 +70,7 @@ class MainActivity : AppCompatActivity() {
 
         allData.forEach { recipe ->
             Log.i("testing", "test: " + recipe.recipeName)
+            Log.i("testing", "test: " + recipe.yield)
         }
         return allData
     }
